48aa62e5a23bc46628b03eedf41372a951efc3dd
Yuri Tihonov <tihonov-yuri@mail.ru>
2026-04-07 19:20:10 +0300

    Move logic from persistence to usecases

--- src/Usecase/TagEntry.hs
+++ src/Usecase/TagEntry.hs
@@ -4,6 +4,7 @@ module Usecase.TagEntry (tagEntry) where
 
 import qualified Data.Text as T
 import Domain.Entry
+import Domain.Tag
 import qualified Persistence.Sqlite as Persistence
 
 tagEntry :: EntryID -> T.Text -> IO (Either T.Text ())
@@ -12,8 +13,12 @@ tagEntry entryid tagname = do
   case entry of
     Left err -> return $ Left err
     Right _ -> do
-      result <- Persistence.getOneTag tagname
-      _ <- case result of
-        Left _ -> Persistence.newTag tagname
-        Right _ -> return $ Right () -- pass
-      Persistence.tagEntry entryid tagname
+      checktag <- Persistence.getOneTag tagname
+      case checktag of
+        Right (Tag tagid _) -> Persistence.tagEntry entryid tagid
+        Left _ -> do
+          _ <- Persistence.newTag tagname
+          newtag <- Persistence.getOneTag tagname
+          case newtag of
+            Left err -> return $ Left err
+            Right (Tag tagid _) -> Persistence.tagEntry entryid tagid
