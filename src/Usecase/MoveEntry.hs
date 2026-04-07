9e1ae3aa39106bd30be4c59d145913c15f2b96ed
sokomadeyo1 <tihonov-yuri@mail.ru>
2026-04-07 15:04:18 +0300

    Move logic of adding and moving entries from persistence to the usecases (#13)

--- src/Usecase/MoveEntry.hs
+++ src/Usecase/MoveEntry.hs
@@ -2,11 +2,16 @@ module Usecase.MoveEntry (moveEntry) where
 
 import qualified Data.Text as T
 import Domain.Entry
+import Domain.Column
 import qualified Persistence.Sqlite as Persistence
 
 moveEntry :: EntryID -> T.Text -> IO (Either T.Text ())
-moveEntry entryID colName = do
-  entry <- Persistence.getOneEntry entryID
+moveEntry entryid colName = do
+  entry <- Persistence.getOneEntry entryid
   case entry of
     Left err -> return $ Left err
-    Right _ -> Persistence.moveEntry entryID colName
+    Right _ -> do
+      col <- Persistence.getOneColumn colName
+      case col of
+        Left err -> return $ Left err
+        Right (Column colid _) -> Persistence.moveEntry entryid colid
