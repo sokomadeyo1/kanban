3de3d2e4e1771c4ac70364624dc59ebd51b77d9f
Yuri Tihonov <tihonov-yuri@mail.ru>
2026-04-08 20:39:13 +0300

    Move getting tagged entries to the GetEntries module

--- src/Usecase/GetEntries.hs
+++ src/Usecase/GetEntries.hs
@@ -4,11 +4,12 @@ module Usecase.GetEntries (getEntries) where
 
 import qualified Data.Text as T
 import Domain.Entry
+import Domain.Tag
 import qualified Persistence.Sqlite as Persistence
 
-getEntries :: IO (Either T.Text [Entry])
+getEntries :: IO (Either T.Text [(Entry, [Tag])])
 getEntries = do
-  entries <- Persistence.getEntries
+  entries <- Persistence.getTaggedEntries
   case entries of
     Right [] -> return $ Left "The board is still empty. Try using \"AddEntry\""
-    _ -> return $ entries
+    _ -> return entries
