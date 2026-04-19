3a04de92d44d33e688c5240f3bf9f26fe35ce155
Yuri Tihonov <tihonov-yuri@mail.ru>
2026-04-20 01:24:20 +0300

    Add all basic tag operations, like with columns

--- src/Handler/Tags.hs
+++ src/Handler/Tags.hs
@@ -1,12 +1,13 @@
+{-
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Handler.Tags (getTagsR) where
 
+import Domain.Tag
 import Foundation
 import Usecase.GetTags
-import Util.PrettyPrint
 import Yesod
 
 getTagsR :: Handler Html
@@ -15,3 +16,47 @@ getTagsR = defaultLayout $ do
   case result of
     Left _ -> [whamlet||]
     Right tags -> $(whamletFile "templates/tags.hamlet")
+-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Handler.Tags (getTagsR, postTagsR) where
+
+import qualified Data.Text as T
+import Domain.Tag
+import Foundation
+import Usecase.GetTags
+import Usecase.NewTag
+import Yesod
+
+tagForm :: Html -> MForm Handler (FormResult T.Text, Widget)
+tagForm = renderDivs $ areq textField "Tag name" Nothing
+
+getTagsR :: Handler Html
+getTagsR = do
+  tagResult <- liftIO getTags
+  tags <- case tagResult of
+    Left _ -> return []
+    Right tags -> return tags
+
+  ((_, widget), enctype) <- runFormPost tagForm
+
+  defaultLayout $(whamletFile "templates/tags.hamlet")
+
+postTagsR :: Handler Html
+postTagsR = do
+  ((formRes, widget), enctype) <- runFormPost tagForm
+  case formRes of
+    FormMissing -> return ()
+    FormFailure _ -> return ()
+    FormSuccess q -> do
+      _ <- liftIO $ newTag q
+      return ()
+
+  tagRes <- liftIO getTags
+  tags <- case tagRes of
+    Left _ -> return []
+    Right tags -> return tags
+
+  defaultLayout $(whamletFile "templates/tags.hamlet")
