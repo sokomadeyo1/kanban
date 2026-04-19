b417e9667aece4dd2db5e18ca250bcae261e1768
Yuri Tihonov <tihonov-yuri@mail.ru>
2026-04-20 02:18:41 +0300

    Check for entry existence before setting its tags

--- src/Usecase/SetEntryTags.hs
+++ src/Usecase/SetEntryTags.hs
@@ -8,17 +8,21 @@ import Data.Either (lefts)
 
 setEntryTags :: EntryID -> [T.Text] -> IO (Either T.Text ())
 setEntryTags entryid tagnames = do
-  gettags <- Persistence.getEntriesTags entryid
-  case gettags of
+  checkentry <- Persistence.getOneEntry entryid
+  case checkentry of
     Left e -> return $ Left e
-    Right entrytags -> do
-      del <- if (entrytags /= []) 
-        then Persistence.removeEntryTags entryid
-        else return $ Right ()
-      case del of
+    Right _ -> do
+      gettags <- Persistence.getEntriesTags entryid
+      case gettags of
         Left e -> return $ Left e
-        Right _ -> do
-          res <- sequence $ map (tagEntry entryid) tagnames
-          case lefts res of
-            [] -> return $ Right ()
-            errs -> return $ Left $ T.unlines errs
+        Right entrytags -> do
+          del <- if (entrytags /= []) 
+            then Persistence.removeEntryTags entryid
+            else return $ Right ()
+          case del of
+            Left e -> return $ Left e
+            Right _ -> do
+              res <- sequence $ map (tagEntry entryid) tagnames
+              case lefts res of
+                [] -> return $ Right ()
+                errs -> return $ Left $ T.unlines errs
