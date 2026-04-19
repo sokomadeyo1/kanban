e4f407908d4c7f8ce209583f69258176c900e527
Yuri Tihonov <tihonov-yuri@mail.ru>
2026-04-19 22:30:09 +0300

    Add form for creating new columns

--- src/Handler/Columns.hs
+++ src/Handler/Columns.hs
@@ -2,16 +2,36 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-module Handler.Columns (getColumnsR) where
+module Handler.Columns (getColumnsR, postColumnsR) where
 
+import qualified Data.Text as T
 import Domain.Column
 import Foundation
 import Usecase.GetColumns
 import Yesod
+import Usecase.NewColumn
+
+postForm :: Html -> MForm Handler (FormResult T.Text, Widget)
+postForm = renderDivs $ areq textField "Column Name" Nothing
 
 getColumnsR :: Handler Html
-getColumnsR = defaultLayout $ do
+getColumnsR = do
+  ((_, widget), enctype) <- runFormPost postForm
   result <- liftIO getColumns
   case result of
-    Left _ -> [whamlet||]
-    Right columns -> $(whamletFile "templates/columns.hamlet")
+    Left _ -> defaultLayout [whamlet||]
+    Right columns -> defaultLayout $(whamletFile "templates/columns.hamlet")
+
+postColumnsR :: Handler Html
+postColumnsR = do
+  ((formRes, widget), enctype) <- runFormPost postForm
+  case formRes of
+    FormMissing -> return ()
+    FormFailure _ -> return ()
+    FormSuccess q -> do
+      _ <- liftIO $ newColumn q
+      return ()
+  result <- liftIO getColumns
+  case result of
+    Left _ -> defaultLayout [whamlet||]
+    Right columns -> defaultLayout $(whamletFile "templates/columns.hamlet")
