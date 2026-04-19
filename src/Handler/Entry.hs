eb5bab8e2b52d11663feb1948ab0a1988d5b34e6
Yuri Tihonov <tihonov-yuri@mail.ru>
2026-04-19 20:35:38 +0300

    Add a dropdown list to the entry page for moving between columns

--- src/Handler/Entry.hs
+++ src/Handler/Entry.hs
@@ -4,30 +4,35 @@
 
 module Handler.Entry (getEntryR, postEntryR) where
 
+import qualified Data.Text as T
+import Domain.Column
 import Domain.Entry
 import Domain.Tag
 import Foundation
-import Usecase.GetOneEntry
-import Usecase.RenameEntry
 import Usecase.EditEntry
-import Util.PrettyPrint
+import Usecase.GetColumns
+import Usecase.GetOneEntry
+import Usecase.MoveEntry
+import Usecase.RenameEntry
 import Util.Cast (maybeToMonoid)
+import Util.PrettyPrint
 import Yesod
 
 getid :: Entry -> Int
 getid = (\(EntryID i) -> i) . entryID
 
-entryFormGen :: Entry -> Html -> MForm Handler (FormResult Entry, Widget)
-entryFormGen entry =
+entryFormGen :: Entry -> [T.Text] -> Html -> MForm Handler (FormResult Entry, Widget)
+entryFormGen entry columns =
   let
     id_ = entryID entry
-    col_ = entryColName entry
-    entryGen = (\title desc -> Entry id_ title (maybeToMonoid desc) col_)
+    entryGen = (\title desc col -> Entry id_ title (maybeToMonoid desc) col)
+    columnlist = fmap (\x -> (x, x)) columns
    in
     renderDivs $
       entryGen
         <$> areq textField "Title"       (Just $        entryTitle entry)
         <*> aopt textField "Description" (Just $ Just $ entryDesc  entry)
+        <*> areq (selectFieldList columnlist) "Column" (Just $ entryColName entry)
 
 getEntryR :: Int -> Handler Html
 getEntryR i = do
@@ -35,9 +40,14 @@ getEntryR i = do
   case res of
     Left _ -> defaultLayout [whamlet||]
     Right (entry, tags) -> do
-      let formGen = entryFormGen entry
-      ((_, widget), enctype) <- runFormPost formGen
-      defaultLayout $(whamletFile "templates/entry.hamlet")
+      getcols <- liftIO getColumns
+      case getcols of
+        Left _ -> defaultLayout [whamlet||]
+        Right cols -> do
+          let colnames = map columnTitle cols
+          let formGen = entryFormGen entry colnames
+          ((_, widget), enctype) <- runFormPost formGen
+          defaultLayout $(whamletFile "templates/entry.hamlet")
 
 postEntryR :: Int -> Handler Html
 postEntryR i = do
@@ -45,13 +55,19 @@ postEntryR i = do
   case res of
     Left _ -> defaultLayout [whamlet||]
     Right (entry_, tags) -> do
-      let formGen = entryFormGen entry_
-      ((formRes, widget), enctype) <- runFormPost formGen
-      case formRes of
-        FormMissing -> defaultLayout [whamlet||]
-        FormFailure _ -> defaultLayout [whamlet||]
-        FormSuccess q -> do
-          _ <- liftIO $ renameEntry (EntryID i) (entryTitle q)
-          _ <- liftIO $ editEntry (EntryID i) (entryDesc q)
-          let entry = Entry { entryID = entryID entry_ , entryTitle = entryTitle q , entryDesc = entryDesc q , entryColName = entryColName entry_ }
-          defaultLayout $(whamletFile "templates/entry.hamlet")
+      getcols <- liftIO getColumns
+      case getcols of
+        Left _ -> defaultLayout [whamlet||]
+        Right cols -> do
+          let colnames = map columnTitle cols
+          let formGen = entryFormGen entry_ colnames
+          ((formRes, widget), enctype) <- runFormPost formGen
+          case formRes of
+            FormMissing -> defaultLayout [whamlet||]
+            FormFailure _ -> defaultLayout [whamlet||]
+            FormSuccess q -> do
+              _ <- liftIO $ renameEntry (EntryID i) (entryTitle q)
+              _ <- liftIO $ editEntry (EntryID i) (entryDesc q)
+              _ <- liftIO $ moveEntry (EntryID i) (entryColName q)
+              let entry = Entry { entryID = entryID entry_ , entryTitle = entryTitle q , entryDesc = entryDesc q , entryColName = entryColName entry_ }
+              defaultLayout $(whamletFile "templates/entry.hamlet")
