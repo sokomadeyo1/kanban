0cd5cfb710ab6c82363b817a7eb598a05a8be45a
Yuri Tihonov <tihonov-yuri@mail.ru>
2026-04-20 15:11:09 +0300

    Add checks before renaming tags

--- src/Usecase/RenameTag.hs
+++ src/Usecase/RenameTag.hs
@@ -1,12 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Usecase.RenameTag (renameTag) where
 
 import qualified Data.Text as T
 import Domain.Tag
 import qualified Persistence.Sqlite as Persistence
+import Util.PrettyPrint
+import Util.Class (dummy)
 
 renameTag :: T.Text -> T.Text -> IO (Either T.Text ())
 renameTag oldname newname = do
-  tag <- Persistence.getOneTag oldname
-  case tag of
-    Left err -> return $ Left err
-    Right (Tag tagid _) -> Persistence.renameTag tagid newname
+  checkCollision <- Persistence.getOneTag newname
+  case checkCollision of
+    Right _ -> return $ Left $ T.unwords ["Tag", pretty (dummy newname :: Tag), "already exists"]
+    Left _ -> do
+      tag <- Persistence.getOneTag oldname
+      case tag of
+        Left err -> return $ Left err
+        Right (Tag tagid _) -> Persistence.renameTag tagid newname
