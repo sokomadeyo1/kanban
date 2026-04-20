0b7b88cbe0e627dbcfac8a2bbb27a012ca0710fd
Yuri Tihonov <tihonov-yuri@mail.ru>
2026-04-20 14:13:27 +0300

    Add checks before renaming columns

--- src/Usecase/RenameColumn.hs
+++ src/Usecase/RenameColumn.hs
@@ -1,12 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Usecase.RenameColumn (renameColumn) where
 
 import qualified Data.Text as T
 import Domain.Column
 import qualified Persistence.Sqlite as Persistence
+import Util.Class (dummy)
+import Util.PrettyPrint
 
 renameColumn :: T.Text -> T.Text -> IO (Either T.Text ())
 renameColumn oldname newname = do
-  col <- Persistence.getOneColumn oldname
-  case col of
-    Left err -> return $ Left err
-    Right (Column colid _) -> Persistence.renameColumn colid newname
+  checkCollision <- Persistence.getOneColumn newname
+  case checkCollision of
+    Right _ -> return $ Left $ T.unwords ["Column", pretty (dummy newname :: Column), "already exists"]
+    Left _ -> do
+      col <- Persistence.getOneColumn oldname
+      case col of
+        Left err -> return $ Left err
+        Right (Column colid _) -> Persistence.renameColumn colid newname
