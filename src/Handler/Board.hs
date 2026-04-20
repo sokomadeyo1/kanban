64e975210edb5aada17c1c0672645c228ef03fc8
Yuri Tihonov <tihonov-yuri@mail.ru>
2026-04-20 17:10:19 +0300

    Display board by columns

--- src/Handler/Board.hs
+++ src/Handler/Board.hs
@@ -12,8 +12,10 @@ import Usecase.GetEntries
 import Usecase.NewEntry
 import Util.Cast (getid, maybeToMonoid)
 import Util.PrettyPrint
+import Util.Util (columnEntries)
 import Yesod
 import Error
+import Data.List (nub)
 
 postForm :: Html -> MForm Handler (FormResult (T.Text, T.Text), Widget)
 postForm =
@@ -32,6 +34,9 @@ getBoardR = do
   entries <- case res of
     Left _ -> return []
     Right entries -> return entries
+
+  let columns = nub $ map (entryColName . fst) entries
+  let entriesByColumn = map (\column -> (column, columnEntries column entries)) columns
   defaultLayout $(whamletFile "templates/board.hamlet")
 
 postBoardR :: Handler Html
@@ -52,4 +57,6 @@ postBoardR = do
   errW <- case err of
     Nothing -> return mempty
     Just (errMsg, errDesc) -> return $ errorWidget errMsg errDesc
+  let columns = nub $ map (entryColName . fst) entries
+  let entriesByColumn = map (\column -> (column, columnEntries column entries)) columns
   defaultLayout $ errW <> $(whamletFile "templates/board.hamlet")
