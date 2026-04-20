3ac85f4da049e6ca5c8bcb44515a2d22054ec3b8
Yuri Tihonov <tihonov-yuri@mail.ru>
2026-04-20 17:34:01 +0300

    Prettyprint tags for better MVP display

--- src/Handler/Board.hs
+++ src/Handler/Board.hs
@@ -6,7 +6,6 @@ module Handler.Board (getBoardR, postBoardR) where
 
 import qualified Data.Text as T
 import Domain.Entry
-import Domain.Tag
 import Foundation
 import Usecase.GetEntries
 import Usecase.NewEntry
