513fafbe88aaa3730695d0d835baab4096c2719d
sokomadeyo1 <tihonov-yuri@mail.ru>
2026-04-23 09:53:13 +0300

    Fix restricting logic (#47)
    Check if columns are the same

--- src/Usecase/RestrictMove.hs
+++ src/Usecase/RestrictMove.hs
@@ -10,22 +10,25 @@ import Util.Class
 
 restrictMove :: T.Text -> T.Text -> IO (Either T.Text ())
 restrictMove fromcolname tocolname = do
-  fromcol <- Persistence.getOneColumn fromcolname
-  case fromcol of
-    Left err -> return $ Left err
-    Right (Column fromcolid _) -> do
-      tocol <- Persistence.getOneColumn tocolname
-      case tocol of
+  if fromcolname == tocolname
+    then return $ Left $ "Matching column names"
+    else do
+      fromcol <- Persistence.getOneColumn fromcolname
+      case fromcol of
         Left err -> return $ Left err
-        Right (Column tocolid _) -> do
-          check <- Persistence.checkRestrict fromcolid tocolid
-          case check of
+        Right (Column fromcolid _) -> do
+          tocol <- Persistence.getOneColumn tocolname
+          case tocol of
             Left err -> return $ Left err
-            Right True -> return $ Left $ T.unwords
-              [ "Moving from"
-              , pretty $ (dummy fromcolname :: Column)
-              , "to"
-              , pretty $ (dummy tocolname :: Column)
-              , "is already restricted"
-              ]
-            Right False -> Persistence.restrictMove fromcolid tocolid
+            Right (Column tocolid _) -> do
+              check <- Persistence.checkRestrict fromcolid tocolid
+              case check of
+                Left err -> return $ Left err
+                Right True -> return $ Left $ T.unwords
+                  [ "Moving from"
+                  , pretty $ (dummy fromcolname :: Column)
+                  , "to"
+                  , pretty $ (dummy tocolname :: Column)
+                  , "is already restricted"
+                  ]
+                Right False -> Persistence.restrictMove fromcolid tocolid
